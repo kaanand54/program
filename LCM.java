77ab238a216e7adabc134905dafce2b69d39c4ef
kaanand54 <kaanand54@users.noreply.github.com>
2016-09-11 12:54:43 +0530

    GCD.java has been used

---
+++ LCM.java
@@ -0,0 +1,13 @@
+// gcd(long a, long b)  is already defined under GCD.java file
+
+private static long lcm(long a, long b)
+{
+    return a * (b / gcd(a, b));
+}
+
+private static long lcm(long[] input)
+{
+    long result = input[0];
+    for(int i = 1; i < input.length; i++) result = lcm(result, input[i]);
+    return result;
+}
